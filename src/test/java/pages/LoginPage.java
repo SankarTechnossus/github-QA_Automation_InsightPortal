967f852838c5fe27a1c4c02d8591270064d94fda
SankarVenkatesan <shankarvenkatesan59@gmail.com>
2025-06-16 16:25:28 +0530

    init commit 03

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,38 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+
+    WebDriver driver;
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators (Update these based on actual site)
+    By usernameInput = By.id("username"); // use actual ID
+    By passwordInput = By.id("password");
+    By loginButton   = By.id("loginBtn");
+
+    // Methods
+    public void enterUsername(String username) {
+        driver.findElement(usernameInput).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordInput).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,32 @@
 package tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import pages.LoginPage;
+import utils.DriverFactory;
+
 public class LoginTest {
+
+    WebDriver driver;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.initDriver();
+        driver.get("https://austin-insight4.partners.org/"); // 🔁 Use actual login URL
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    public void testLogin() {
+        loginPage.login("your-username", "your-password");  // 🔁 Use real credentials
+        // Add assertions or validations here
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
 }
