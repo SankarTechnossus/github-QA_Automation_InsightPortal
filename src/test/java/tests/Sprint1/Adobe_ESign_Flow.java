3d458d6a3c64f6c5611f91eb95d4f9c3ff3ec268
SankarVenkatesan <shankarvenkatesan59@gmail.com>
2025-06-26 20:00:34 +0530

    init commit 14

--- src/test/java/tests/Sprint1/Adobe_ESign_Flow.java
+++ src/test/java/tests/Sprint1/Adobe_ESign_Flow.java
@@ -24,239 +24,242 @@ public class Adobe_ESign_Flow {
 
     @BeforeSuite
     public void setupExtentReport() {
-        // 'Create the directory where the HTML report will be stored'
+        // User will create the directory where the HTML report will be stored, if it does not already exist
         new File("test_reports").mkdirs();
 
-        // 'Initialize ExtentSparkReporter for HTML report generation'
+        // User will initialize ExtentSparkReporter to generate the HTML test execution report
         ExtentSparkReporter htmlReporter = new ExtentSparkReporter("test_reports/Adobe_ESign_Flow.html");
 
-        // 'Set report document title and name'
+        // User will set the document title and report name for the test report
         htmlReporter.config().setDocumentTitle("Adobe_ESign_Flow");
         htmlReporter.config().setReportName("Sprint 1 Automation");
 
-        // 'Attach the configured reporter to ExtentReports instance'
+        // User will attach the reporter to the ExtentReports instance
         extent = new ExtentReports();
         extent.attachReporter(htmlReporter);
     }
 
     @BeforeMethod
     public void setupBrowser() {
-        // 'Setup ChromeDriver using WebDriverManager'
+        // User will setup and configure the Chrome WebDriver using WebDriverManager
         WebDriverManager.chromedriver().setup();
 
-        // 'Launch Chrome browser'
+        // User will launch a new Chrome browser instance
         driver = new ChromeDriver();
 
-        // 'Maximize browser window'
+        // User will maximize the browser window to ensure all UI elements are visible
         driver.manage().window().maximize();
 
-        // 'Set WebDriver wait timeout to 10 seconds'
+        // User will initialize explicit wait with a timeout of 10 seconds for dynamic element handling
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @Test
     public void createFormFlow() {
-        // 'Start a new test case in Extent Report'
+        // User will create a new test entry in the Extent Report for the form creation flow
         test = extent.createTest("Forms_Management_Create_New_Form Test");
 
         try {
-            // 'Navigate to the Insight portal URL'
+            // User will open the login page of the Insight Portal application
             driver.get("https://sacramento-insight4.partners.org/");
             test.info("Opened dashboard URL");
 
-            // 'Wait for login page to load'
+            // User will wait for the login screen to load completely before performing actions
             Thread.sleep(20000);
 
-            // 'Enter username'
-            WebElement username = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("input28")));
+            // User will enter the username into the username input field
+            WebElement username = driver.findElement(By.xpath("//input[@id='input28']"));
             username.sendKeys("HS131");
             test.pass("Entered username");
 
-            // 'Click Next button'
+            // User will click the 'Next' button to proceed to the password entry screen
             WebElement nextBtn = driver.findElement(By.xpath("//input[@value='Next']"));
             nextBtn.click();
             test.pass("Clicked Next");
 
-            // 'Wait for password field to appear'
+            // User will wait briefly for the password field to appear
             Thread.sleep(5000);
 
-            // 'Enter password'
-            WebElement password = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("credentials.passcode")));
+            // User will input the user's password into the password field
+            WebElement password = driver.findElement(By.xpath("//input[@name='credentials.passcode']"));
             password.sendKeys("MGBexport2025#");
             test.pass("Entered password");
 
-            // 'Click Verify button'
+            // User will pause briefly before attempting to click the 'Verify' button
             Thread.sleep(5000);
+
+            // User will click the 'Verify' button to authenticate the user
             WebElement verifyBtn = driver.findElement(By.xpath("//input[@value='Verify']"));
             verifyBtn.click();
             test.pass("Clicked Verify");
 
-            // 'Wait for dashboard to load'
+            // User will wait for the user to be redirected to the dashboard
             Thread.sleep(30000);
 
-            // 'Click on Agreements link from sidebar'
+            // User will click the 'Agreements' link from the sidebar menu
             WebElement agreementsLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/agreements' and contains(.,'Agreements')]")));
             agreementsLink.click();
             test.pass("Clicked Agreements link");
 
-            // 'Wait for agreements page to load'
+            // User will wait for the Agreements page to load
             Thread.sleep(20000);
 
-            // 'Enter Agreement Number'
-            WebElement agreementNumberInput = wait.until(ExpectedConditions.elementToBeClickable(By.id("agreementNumber")));
+            // User will enter the agreement number into the corresponding input field
+            WebElement agreementNumberInput = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='agreementNumber']")));
             agreementNumberInput.sendKeys("2025A011754");
             test.pass("Entered Agreement Number: 2025A011754");
 
-            // 'Wait before clicking Search'
+            // User will wait after entering the agreement number
             Thread.sleep(10000);
 
-            // 'Click Search button'
+            // User will click the 'Search' button to search for the agreement
             WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Search']")));
             searchButton.click();
             test.pass("Clicked Search button");
 
-            // 'Wait for search results to load'
+            // User will wait for search results to appear
             Thread.sleep(20000);
 
-            // 'Click on Agreement ID from search results'
+            // User will click on the agreement number span to open agreement details
             WebElement agreementSpan = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='2025A011754']")));
             agreementSpan.click();
             test.pass("Clicked on Agreement span: 2025A011754");
 
-            // 'Wait after clicking Agreement span'
+            // User will wait after opening the agreement
             Thread.sleep(10000);
 
-            // 'Click Deliverables tab from sidebar'
+            // User will click the 'Deliverables' tab in the sidebar for the selected agreement
             WebElement deliverablesTab = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/agreements/2025A011754/latest/deliverables']//span[text()='Deliverables']")));
             deliverablesTab.click();
             test.pass("Clicked on correct Deliverables tab in sidebar");
 
-            // 'Wait after clicking Deliverables tab'
+            // User will wait after navigating to the Deliverables tab
             Thread.sleep(5000);
 
-            // 'Minimize E-Sign section'
+            // User will click the E-Sign section to minimize it
             WebElement eSignSection = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='title-text' and contains(., 'E-Sign')]")));
             eSignSection.click();
             test.pass("Clicked on E-Sign minimise");
 
-            // 'Wait after minimizing E-Sign section'
+            // User will wait after minimizing the E-Sign section
             Thread.sleep(10000);
 
-            // 'Maximize E-Sign section again'
+            // User will click the E-Sign section to maximize it
             WebElement eSignSection1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='title-text' and contains(., 'E-Sign')]")));
             eSignSection1.click();
             test.pass("Clicked on E-Sign section Maximise");
 
-            // 'Wait after expanding E-Sign section'
+            // User will wait after maximizing the E-Sign section
             Thread.sleep(5000);
 
-            // 'Click specific E-Sign agreement link'
+            // User will click the 'For fully executed agreement' link under E-Sign section
             WebElement eSignAgreementLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/agreements/2025A011754/latest/deliverables/1110376']//span[text()='For fully executed agreement']")));
             eSignAgreementLink.click();
             test.pass("Clicked 'For fully executed agreement' under E-Sign section");
 
-            // 'Wait after clicking agreement link'
+            // User will wait after clicking the agreement link
             Thread.sleep(10000);
 
-            // 'Click Adobe integration icon'
+            // User will click the Adobe integration icon to start the e-sign process
             WebElement adobeIcon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@alt='adobe-icon']")));
             adobeIcon.click();
             test.pass("Clicked Adobe integration icon");
 
-            // 'Wait after Adobe integration icon click'
+            // User will wait after clicking the Adobe icon
             Thread.sleep(10000);
 
-            // 'Upload agreement file'
+            // User will upload the agreement PDF file via file input
             WebElement fileInput = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='file' and @accept='application/pdf']")));
             fileInput.sendKeys("C:\\Users\\SankarVenkatesan\\OneDrive - Technossus LLC\\Documents\\Agreement Details\\Agreement Info 2025_03.pdf");
             test.pass("Successfully uploaded 'Agreement Info 2025.pdf'");
 
-            // 'Wait after file upload'
+            // User will wait after uploading the file
             Thread.sleep(10000);
 
-            // 'Enter Agreement Name'
+            // User will enter the name of the agreement into the input field
             WebElement inputField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@class='default-input _full-width text-input default-input' and @type='text']")));
             inputField.sendKeys("QA_Automation_Agreement_information3");
             test.pass("Entered text in Agreement name");
 
-            // 'Wait before adding recipient'
+            // User will wait after entering agreement name
             Thread.sleep(10000);
 
-            // 'Click Add Recipient button'
+            // User will click the 'Add Recipient' button
             WebElement addRecipientButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Add Recipient']")));
             addRecipientButton.click();
             test.pass("Clicked 'Add Recipient' button");
 
-            // 'Wait before entering recipient details'
+            // User will wait after adding recipient section
             Thread.sleep(10000);
 
-            // 'Enter recipient email address'
+            // User will enter the recipient email in the input field
             WebElement fullWidthInput = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Email']/following::input[@type='text'][1]")));
             fullWidthInput.sendKeys("Sankar.Venkatesan@technossus.com");
             test.pass("Entered value in recipient input field");
 
-            // 'Wait before previewing document'
+            // User will wait after entering recipient email
             Thread.sleep(10000);
 
-            // 'Click Preview Document button'
+            // User will click the 'Preview Document' button
             WebElement previewButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Preview Document']")));
             previewButton.click();
             test.pass("Clicked 'Preview Document' button");
 
-            // 'Wait for preview to load'
+            // User will wait after previewing the document
             Thread.sleep(50000);
 
-//            // 'Click E-signature field'
-//            WebElement eSignButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='E-signature']/ancestor::div[@class='sc-ejwLJJ gmtySD']")));
-//            eSignButton.click();
-//            test.pass("Clicked 'E-signature' field button");
-//
-//            // 'Wait after E-signature click'
-//            Thread.sleep(10000);
-//
-//            // 'Click Send button'
-//            WebElement sendButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Send' and @class='ntVziG_spectrum-Button-label']")));
-//            sendButton.click();
-//            test.pass("Clicked 'Send' button");
-//
-//            // 'Wait after clicking Send'
-//            Thread.sleep(10000);
-//
-//            // 'Click Continue button'
-//            WebElement continueButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Continue' and @class='ntVziG_spectrum-Button-label']")));
-//            continueButton.click();
-//            test.pass("Clicked 'Continue' button");
-//
-//            // 'Wait after clicking Continue'
-//            Thread.sleep(10000);
+            // User will click on the E-signature field in the preview section
+            WebElement eSignButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='E-signature']/ancestor::div[@class='sc-ejwLJJ gmtySD']")));
+            eSignButton.click();
+            test.pass("Clicked 'E-signature' field button");
 
-            // 'Click Status tab'
+            // User will wait after clicking E-signature field
+            Thread.sleep(10000);
+
+            // User will click the 'Send' button to send the document for signature
+            WebElement sendButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Send' and @class='ntVziG_spectrum-Button-label']")));
+            sendButton.click();
+            test.pass("Clicked 'Send' button");
+
+            // User will wait after clicking send button
+            Thread.sleep(10000);
+
+            // User will click the 'Continue' button to confirm sending
+            WebElement continueButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Continue' and @class='ntVziG_spectrum-Button-label']")));
+            continueButton.click();
+            test.pass("Clicked 'Continue' button");
+
+            // User will wait after clicking continue button
+            Thread.sleep(10000);
+
+            // User will click the 'Status' tab to verify the current document status
             WebElement statusTab = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Status']")));
             statusTab.click();
             test.pass("Clicked 'Status' tab");
 
-            // 'Wait after clicking Status tab'
+            // User will wait after navigating to the status tab
             Thread.sleep(10000);
 
         } catch (Exception e) {
-            // 'Log the exception in the test report'
+            // User will capture and log any exceptions that occur during the test
             test.fail("Test failed due to exception: " + e.getMessage());
         }
     }
 
     @AfterMethod
     public void tearDown() {
-        // 'Close the browser after test execution'
+        // User will close the browser instance after test execution is complete
         if (driver != null) {
             driver.quit();
         }
-        // 'Log browser closure in the test report'
+
+        // User will record browser closure in the test report
         test.info("Browser was successfully closed.");
     }
 
     @AfterSuite
     public void flushExtentReport() {
-        // 'Generate the final HTML test execution report'
+        // User will flush all the test information and generate the final HTML report
         extent.flush();
     }
 }
