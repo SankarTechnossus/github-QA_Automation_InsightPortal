6840efbc72b6386aeafb17a981ae9bf991847366
SankarVenkatesan <shankarvenkatesan59@gmail.com>
2025-08-26 15:03:45 +0530

    init commit - Data_Aug_26

--- src/test/java/tests/AdobeEsign/Sprint3/EsignatureReport.java
+++ src/test/java/tests/AdobeEsign/Sprint3/EsignatureReport.java
@@ -145,7 +145,6 @@ import java.time.Duration;
 
 
 
-
             } catch (Exception e) {
                 // User will capture and log any exceptions that occur during the test
                 ExtentReportListener.getExtentTest().fail("Test failed due to exception: " + e.getMessage());
