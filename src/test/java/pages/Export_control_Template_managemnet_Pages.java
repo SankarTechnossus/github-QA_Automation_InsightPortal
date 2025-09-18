1863ae48a108e80bbded408e29ff5d9bf1dc70a9
SankarVenkatesan <shankarvenkatesan59@gmail.com>
2025-09-18 17:21:01 +0530

    init commit - Date 18-Sep-2025

--- src/test/java/pages/Export_control_Template_managemnet_Pages.java
+++ src/test/java/pages/Export_control_Template_managemnet_Pages.java
@@ -1,4 +1,25 @@
 package pages;
 
-public class Export_control_Template_managemnet_Pages {
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class Export_control_Template_managemnet_Pages extends BasePage {
+
+    public Export_control_Template_managemnet_Pages(WebDriver driver) {
+        super(driver);
+    }
+
+
+
+    //Locators
+
+
+    //Action
+
+
+
+
+
+
+
 }
