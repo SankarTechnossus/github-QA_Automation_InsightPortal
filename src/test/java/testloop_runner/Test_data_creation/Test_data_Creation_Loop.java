44df09415958cfea9af96401fdc365bb2dcce206
SankarVenkatesan <shankarvenkatesan59@gmail.com>
2026-02-16 13:20:14 +0530

    INIT Commit Test Commit - Security PBI's 1.Organization_Level 2.Profile_level 3.Record_level

--- src/test/java/testloop_runner/Test_data_creation/Test_data_Creation_Loop.java
+++ src/test/java/testloop_runner/Test_data_creation/Test_data_Creation_Loop.java
@@ -107,7 +107,7 @@ public class Test_data_Creation_Loop {
             Assert.assertTrue(dashboardPage.VerifyUserLandsOnDashboardPage());
             ExtentReportListener.getExtentTest().pass("Login successful");
 
-            int recordCount = 2;
+            int recordCount = 1000;
 
             for (int i = 1; i <= recordCount; i++) {
 
